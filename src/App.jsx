c75b50fad651ab1cf12f0662d0b0aa0e72cd8e92
Sharmin64 <lizaeatsbanana@gmail.com>
2024-09-08 10:07:31 +0600

    initial set up done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Header />
+        <Sidebar />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <div>
+      <button className="btn btn-accent text-3xl font-bold underline">
+        click here to see sidebar
+      </button>
+    </div>
+  );
+};
+
+export default Sidebar;
